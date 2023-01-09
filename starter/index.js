482ac053e76c7f216d84367edd02588bafc42d39
niwantha33 <niwantha33@gmail.com>
2023-01-09 19:36:45 +0000

    changed to parm to praram.

--- starter/index.js
+++ starter/index.js
@@ -95,8 +95,8 @@ var finances = [
 ];
 
 /*
-@func   :   Find the total months from "finances" data sets
-@parm   :   Array 
+@brief   :   Find the total months from "finances" data sets
+@param   :   Array 
 @return :   return total months 
 */
 const find_total_months = function (finances) {
@@ -120,8 +120,8 @@ const find_total_months = function (finances) {
 }
 
 /*
-@func   :   Find the Profit/Losses over the entire period from "finances" data sets
-@parm   :   Array 
+@brief   :   Find the Profit/Losses over the entire period from "finances" data sets
+@param   :   Array 
 @return :   return profit/losses
 */
 const find_profit_losses = function (finances) {
@@ -139,9 +139,9 @@ const find_profit_losses = function (finances) {
 
 
 /*
-@func   :   Find average of the changes in Profit/Losses over the entire period 
+@brief   :   Find average of the changes in Profit/Losses over the entire period 
             from "finances" data sets
-@parm   :   Array 
+@param   :   Array 
 @return :   return Average Change ((Total/Number of months))
 @note:      You will need to track what the total change in profits are from 
             month to month and then find the average.
@@ -159,9 +159,9 @@ const find_avg_of_change_profit_losses = function (finances) {
 }
 
 /*
-@func   :   Find greatest increase in profits (date and amount) over the entire period 
+@brief   :   Find greatest increase in profits (date and amount) over the entire period 
             from "finances" data sets
-@parm   :   Array 
+@param   :   Array 
 @return :   return array [month,p/l]
 */
 
@@ -185,9 +185,9 @@ const find_greatest_profit_period = function (finances) {
 }
 
 /*
-@func   :   Find greatest decrease in losses (date and amount) over the entire period 
+@brief   :   Find greatest decrease in losses (date and amount) over the entire period 
             from "finances" data sets
-@parm   :   Array 
+@param   :   Array 
 @return :   return array [month,p/l]
 */
 
